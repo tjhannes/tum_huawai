9f2b89be6ad045c7f9be856a1d5c39026c7038a1
Johannes Thanner <johannes.thanner@capgemini.com>
2019-01-18 17:29:32 +0100

    new model

--- app/src/main/java/com/example/johannes/huawei/DangermodelModel.java
+++ app/src/main/java/com/example/johannes/huawei/DangermodelModel.java
@@ -9,11 +9,11 @@ public class DangermodelModel {
 
     /**** user load model manager sync interfaces ****/
     public static int load(AssetManager mgr){
-            return ModelManager.loadModelSync("Dangermodel", mgr);
+            return ModelManager.loadModelSync("Danger", mgr);
     }
 
     public static float[] predict(float[] buf){
-        return ModelManager.runModelSync("Dangermodel",buf);
+        return ModelManager.runModelSync("Danger",buf);
     }
 
     public static int unload(){
@@ -27,11 +27,11 @@ public class DangermodelModel {
     }
 
     public static void loadAsync(AssetManager mgr){
-        ModelManager.loadModelAsync("Dangermodel", mgr);
+        ModelManager.loadModelAsync("Danger", mgr);
     }
 
     public static void predictAsync(float[] buf) {
-        ModelManager.runModelAsync("Dangermodel",buf);
+        ModelManager.runModelAsync("Danger",buf);
     }
 
     public static void unloadAsync(){
